948295c5ac5afbb66761f48d1ac01b31741e7323
Hans Hazairi <hanshazairi@gmail.com>
2021-12-24 16:56:51 +0800

    chore: handle 'exit' builtin

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 12:29:32 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/12/24 16:43:46 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/12/24 16:55:18 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,9 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = readline("minishell $> ");
-		if (line)
-			printf("%s\n", line);
-		else
+		if (!line || !ft_strncmp(line, "exit", ft_strlen(line)))
 			break ;
+		printf("%s\n", line);
 		free(line);
 	}
 	eof();
