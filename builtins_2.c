d2dd62d61bc76a24513515d19e956e8648b0f521
Bunyod16 <bunyodshams@gmail.com>
2022-06-20 12:30:31 +0800

    chore: env builtin

--- builtins_2.c
+++ builtins_2.c
@@ -3,17 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: bunyodshams <bunyodshams@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 22:01:47 by hbaddrul          #+#    #+#             */
-/*   Updated: 2022/04/10 23:53:37 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2022/06/20 12:11:38 by bunyodshams      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdio.h>
 
 void	env(int i, t_shell_info *info)
 {
 	(void)i;
-	(void)info;
+
+	i = 0;
+	while (info->envp[i])
+	{
+		printf("%s\n", info->envp[i++]);
+	}
 }
